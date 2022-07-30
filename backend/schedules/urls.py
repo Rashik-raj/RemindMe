93f42127004f7a6f971246bc1068327f074625f2
rashik-raj <rashik123.rs@gmail.com>
2022-07-30 13:23:33 +0545

    REM-5; create api for schedule

--- backend/schedules/urls.py
+++ backend/schedules/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from schedules.views.schedule import ScheduleViewSet
+
 router = DefaultRouter(trailing_slash=True)
+router.register(r"", ScheduleViewSet, basename="schedule")
 
 urlpatterns = [path("", include(router.urls))]
