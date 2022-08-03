59ca0018119c079f614c1acc58125f31d29093db
rashik-raj <rashik123.rs@gmail.com>
2022-08-03 21:38:13 +0545

    REM-7; added email api for superadmin

--- backend/emails/urls.py
+++ backend/emails/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from emails.views.email import EmailViewSet
+
 router = DefaultRouter(trailing_slash=True)
+router.register(r"", EmailViewSet, basename='email')
 
 urlpatterns = [path("", include(router.urls))]
--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework.permissions import BasePermission
 
 
+class SuperUserPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_superuser
+
+
 class RemindMePermission(BasePermission):
     def has_permission(self, request, view):
         return True
